453845400c231b81c2d76a999abbf9eebbaa16d2
Jola H Batin <jolawepiss@gmail.com>
2023-11-09 14:12:30 +0100

    fix: added rerouting for invalid params in dashboard/users route

--- src/app/dashboard/user/[id]/page.tsx
+++ src/app/dashboard/user/[id]/page.tsx
@@ -3,8 +3,10 @@ import UserDetailBackButton from "./view/back_button";
 import UserDetailIntroSection from "./view/intro_section";
 import UserDetailBodySection from "./view/body_section";
 import UserDetailHeaderWithActions from "./view/header";
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { useRouter } from "next/navigation";
+import useToggle from "@/hooks/useToggle";
+import { UsersContext, UsersContextType } from "@/context/useUsers";
 
 export default function UserDetailsPage({
   params,
@@ -12,15 +14,26 @@ export default function UserDetailsPage({
   params: { id: string };
 }) {
   const router = useRouter();
+  const [toggle, handleToggle] = useToggle();
+  const { users } = useContext(UsersContext) as UsersContextType;
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
-    if (isNaN(Number(params.id)) || Number(params.id) < 1) {
+    //navigate to dashboard if params is invalid
+    if (
+      isNaN(Number(params.id)) ||
+      Number(params.id) < 1 ||
+      Number(params.id) > users.length
+    ) {
       router.push("/dashboard/users");
+    } else {
+      handleToggle();
     }
   }, []);
 
-  return (
+  return !toggle ? (
+    <></>
+  ) : (
     <div className="user-details">
       <UserDetailBackButton />
       <UserDetailHeaderWithActions props={{ id: Number(params.id) }} />
